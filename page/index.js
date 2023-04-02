93ba89e49f8363e2f516cfdb954147be3e7f01c1
misxzaiz <108251808+misxzaiz@users.noreply.github.com>
2023-04-02 14:13:44 +0800

    s

--- page/index.js
+++ page/index.js
@@ -10,18 +10,14 @@ function init(index) {
     .then(function(data) {
         // 获取ul元素
         let ul = document.getElementById('my-list')
-        for(var key in data[index]) {
+        data.index.forEach(function(element) {
             // 创建li元素
             let li = document.createElement('li');
-            // 创造链接元素
-            let a =  document.createElement('a');
-            a.setAttribute('href',data[index][key]);
-            a.textContent = key;
-            // 将链接元素添加到li中
-            li.appendChild(a);
+            li.textContent = element;
             // 将li添加到ul中
             ul.appendChild(li);
-        }
+        });
+ 
     })
     .catch(function(error) {
       console.log('There was a problem with the fetch operation:', error.message);
