a434d2e290cd5f93e415bae0d965089c4c32af4a
misxzaiz <108251808+misxzaiz@users.noreply.github.com>
2023-04-14 15:02:44 +0800

    6666666

--- page/page/index/index.js
+++ page/page/index/index.js
@@ -1,5 +1,5 @@
 function init() {
-  fetch('https://misxzaiz.top/page/page/index/index.json')
+  fetch('https://misxzaiz.github.io/page/page/index/index.json')
   .then(function(response) {
     if (!response.ok) {
       throw new Error('Network response was not ok');
