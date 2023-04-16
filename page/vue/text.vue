c0fc3ed0cb916ec555a351fec11ed9bf22523a3b
misxzaiz <108251808+misxzaiz@users.noreply.github.com>
2023-04-16 18:55:06 +0800

    666666

---
+++ page/vue/text.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li><router-link to="/">首页</router-link></li>
+        <li><router-link to="/about">关于我们</router-link></li>
+        <li><router-link to="/contact">联系我们</router-link></li>
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+}
+</script> 
+
+<!-- 以上代码是一个基本的 Vue 导航栏实例，包含三个路由链接，分别指向首页、关于我们和联系我们。 -->
