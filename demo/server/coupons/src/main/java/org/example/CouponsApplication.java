18b3a7c25b4d17ba1fba9182f7900e0299370b99
chenshangquan <2840927713@qq.com>
2023-06-11 20:39:08 +0800

    更新

---
+++ demo/server/coupons/src/main/java/org/example/CouponsApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CouponsApplication.class,args);
+    }
+}
