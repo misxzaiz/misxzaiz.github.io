2edb9ffe262d40360b4653a96cebabf20bbc5c31
chenshangquan <2840927713@qq.com>
2023-06-15 00:33:58 +0800

    gx

--- demo/index.js
+++ demo/index.js
@@ -1,2 +1,38 @@
 let reqUrl = "http://127.0.0.1"
 let tokenSession = sessionStorage.getItem("token")
+
+// let commonURL = "http://192.168.50.115:8081";
+// let commonURL = "/api";
+// 设置后台服务地址
+axios.defaults.baseURL = reqUrl;
+axios.defaults.timeout = 2000;
+// request拦截器，将用户token放入头中
+let token = sessionStorage.getItem("token");
+axios.interceptors.request.use(
+  config => {
+    if(token) config.headers['authorization'] = token
+    return config
+  },
+  error => {
+    console.log(error)
+    return Promise.reject(error)
+  }
+)
+axios.interceptors.response.use(function (response) {
+    // 判断执行结果
+    if (!response.data.success) {
+      return Promise.reject(response.data.errorMsg)
+    }
+    return response;
+  }, function (error) {
+    // 一般是服务端异常或者网络异常
+    console.log(error)
+    if(error.response.status == 401){
+      // 未登录，跳转
+      setTimeout(() => {
+        location.href = "pages/login/index.html"
+      }, 200);
+      return Promise.reject("请先登录");
+    }
+    return Promise.reject("服务器异常");
+  });
