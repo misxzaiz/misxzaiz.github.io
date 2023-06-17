7f11c4250ebf7c920aaae393fffee15f9c730ac5
chenshangquan <2840927713@qq.com>
2023-06-17 12:55:58 +0800

    gx

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,9 @@
-let reqUrl = "https://xiaozaiz.xyz"
+let reqUrl = "http://xiaozaiz.xyz"
 
 // let reqUrl = "http://192.168.43.244"
 // 设置后台服务地址
 axios.defaults.baseURL = reqUrl;
-axios.defaults.timeout = 2000;
+// axios.defaults.timeout = 2000;
 // request拦截器，将用户token放入头中
 let token = sessionStorage.getItem("token");
 axios.interceptors.request.use(
@@ -16,21 +16,21 @@ axios.interceptors.request.use(
     return Promise.reject(error)
   }
 )
-axios.interceptors.response.use(function (response) {
-    // 判断执行结果
-    if (!response.data.success) {
-      return Promise.reject(response.data.errorMsg)
-    }
-    return response;
-  }, function (error) {
-    // 一般是服务端异常或者网络异常
-    console.log(error)
-    if(error.response.status == 401){
-      // 未登录，跳转
-      setTimeout(() => {
-        location.href = "pages/login/index.html"
-      }, 200);
-      return Promise.reject("请先登录");
-    }
-    return Promise.reject("服务器异常");
-  });
+// axios.interceptors.response.use(function (response) {
+//     // 判断执行结果
+//     if (!response.data.success) {
+//       return Promise.reject(response.data.errorMsg)
+//     }
+//     return response;
+//   }, function (error) {
+//     // 一般是服务端异常或者网络异常
+//     console.log(error)
+//     if(error.response.status == 401){
+//       // 未登录，跳转
+//       setTimeout(() => {
+//         location.href = "pages/login/index.html"
+//       }, 200);
+//       return Promise.reject("请先登录");
+//     }
+//     return Promise.reject("服务器异常");
+//   });
