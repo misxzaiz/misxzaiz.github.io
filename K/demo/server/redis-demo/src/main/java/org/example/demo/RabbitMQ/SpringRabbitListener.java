301e9886c53b5c59b69a70c3d201b4fcd2763aed
chenshangquan <2840927713@qq.com>
2023-06-19 15:40:08 +0800

    gx

--- K/demo/server/redis-demo/src/main/java/org/example/demo/RabbitMQ/SpringRabbitListener.java
+++ K/demo/server/redis-demo/src/main/java/org/example/demo/RabbitMQ/SpringRabbitListener.java
@@ -6,7 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringRabbitListener {
     @RabbitListener(queues = "simple.queue")
-    public void listenSimpleQueueMessage(String msg) {
-        System.out.println("【接收端】simple.queue："+msg);
+    public void listenWorkQueueMessage1(String msg) throws InterruptedException {
+        System.out.println("【接收端】simple.queue(1)："+msg);
+        Thread.sleep(20);
+    }
+
+    @RabbitListener(queues = "simple.queue")
+    public void listenWorkQueueMessage2(String msg) throws InterruptedException {
+        System.err.println("【接收端】simple.queue(2)："+msg);
+        Thread.sleep(200);
     }
 }
