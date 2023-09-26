b5c4148a3c0759b5ad989e06018458bc418c6031
Shangquan <2840927713@qq.com>
2023-09-26 17:25:24 +0800

    gx

--- uni-app/h5/vite.config.ts
+++ uni-app/h5/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  server: {
+    proxy: {
+      '/h5': {
+        target: 'https://misxzaiz.github.io/h5',
+        changeOrigin: true,
+      },
+    }
+  }
 });
