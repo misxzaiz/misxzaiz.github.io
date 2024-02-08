b654e5dc0c5ce98914bb6b228da96675c24d6054
Shangquan <2840927713@qq.com>
2024-02-08 21:26:01 +0800

    fix agree

---
+++ assets/pages-test-test.fd90ee83.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./index-c3d4cf8a.js";const s=e({name:"test.vue",data:()=>({}),methods:{}},[["render",function(e,s,n,o,r,d){return t(),a("div",{class:"container"})}]]);export{s as default};
